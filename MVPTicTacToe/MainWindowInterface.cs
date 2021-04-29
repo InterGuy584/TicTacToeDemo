7bfeb3ab0004964edcc6b20f33816f2338f8a80f
Philippe Lizotte <55700508+InterGuy584@users.noreply.github.com>
2021-04-29 11:44:33 -0400

    made the repository not empty

---
+++ MVPTicTacToe/MainWindowInterface.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGuiMVP
+{
+    public interface MainWindowInterface
+    {
+        void DrawTicTacToeSymbol(int player, int row, int col);
+        void UpdateGameFinished(string text);
+        void ShowNextPlayer(int player);
+        void ShowError(string text);
+        void UpdateStatus(string text);
+        void ResetBoard();
+    }
+}
---
+++ TicTacToeTest/UnitTest1.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TicTacToeGuiMVP;
+
+namespace UnitTestTicTacToe
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public class TestUIInterface : MainWindowInterface
+        {
+            public bool resetBoardCalled = false;
+            public string updataStatusString = "";
+
+            public void DrawTicTacToeSymbol(int player, int row, int col)
+            {
+
+            }
+
+            public void ResetBoard()
+            {
+                resetBoardCalled = true;
+            }
+
+            public void ShowError(string v)
+            {
+
+            }
+
+            public void ShowNextPlayer(int nextPlayer)
+            {
+
+            }
+
+            public void UpdateGameFinished(string v)
+            {
+
+            }
+
+            public void UpdateStatus(string v)
+            {
+                updataStatusString = v;
+            }
+        }
+
+
+        [TestMethod]
+        public void Test_StartGame()
+        {
+            //Arrange
+            TestUIInterface theTestedUI = new TestUIInterface();
+            Presenter thePresenter = new Presenter(theTestedUI);
+
+            //Act
+            thePresenter.StartGame();
+
+            //Assert
+            Assert.IsTrue(theTestedUI.resetBoardCalled);
+            Assert.AreEqual("status: ready", theTestedUI.updataStatusString);
+        }
+
+    }
+}
